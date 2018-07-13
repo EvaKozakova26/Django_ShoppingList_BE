dbccb327e5dc7a6e3a08cffdbe6f41b566911a58
EvaKozakova26 <evkozakova@seznam.cz>
2018-07-13 13:53:39 +0200

    added detail view, index.html with list of items

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from polls.models import Item
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("helou")
+    latest_items_list = Item.objects.order_by('-date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_items_list': latest_items_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, item_id):
+    return HttpResponse("You are looking at item %s." % item_id)
