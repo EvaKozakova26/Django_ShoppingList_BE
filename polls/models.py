fde3e675530607734a9b84197dd0d910c651c3bc
EvaKozakova26 <evkozakova@seznam.cz>
2018-07-13 10:58:26 +0200

    added __str__() to models

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,23 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
 
 class Item(models.Model):
     itemName = models.CharField(max_length=50)
     date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.itemName
+
+    def was_published_recently(self):
+        return self.date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Type(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     typeName = models.CharField(max_length=20);
+
+    def __str__(self):
+        return self.typeName
