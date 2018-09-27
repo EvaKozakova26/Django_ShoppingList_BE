a5a0fb031090802ea61a5f9e32ba7339cef12646
EvaKozakova26 <evkozakova@seznam.cz>
2018-09-27 20:38:05 +0200

    unit test

---
+++ first_django/tests/test_models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from polls.models import Item
+
+
+class ItemTestCase(TestCase):
+
+    def setUp(self):
+        self.item = Item.objects.create(itemName='TestName', date=datetime.now())
+
+    def testGetItems(self):
+        self.assertEquals(Item.objects.get(itemName='TestName').itemName, 'TestName')
